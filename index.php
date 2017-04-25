4484735ef4b9b7af269dda18b689722fd3937164
marcoscareggio <marcoscareggio2005@hotmail.com>
2017-04-25 20:51:13 -0300

    Agregar contenedores para Enlaces

--- index.php
+++ index.php
@@ -3,53 +3,144 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Web de Cosepar Ltda., Arata La Pampa">
+    <meta name="author" content="Marcos Careggio">
     <link rel="icon" href="favicon.ico">
 
     <title>COSEPAR Ltda</title>
 
     <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
     <!-- Custom styles for this template -->
     <link href="css/estilo.css" rel="stylesheet">
+    <!-- Carousel - Diapositivas-->
+    <link rel="stylesheet" href="css/carousel.css">
+    <!-- barra de navegacion -->
+    <link rel="stylesheet" href="css/navbar.css">
+    
+    <script>
+      // Activate CarouselDiapositivas
+      $("#myCarousel").carousel({interval: 500});
+      // Enable Carousel Indicators
+      $(".item").click(function(){
+      $("#myCarousel").carousel(1);
+});
+	  //----Fin Carousel-Diapòsitivas
+    </script>
+
   </head>
 
   <body>
-	
-     <nav class="navbar navbar-default navbar-static-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">COSEPAR</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Inicio</a></li>
-            <li><a href="#about">Nosotros</a></li>
-            <li><a href="#contact">Contacto</a></li>
+  
+     <nav class="navbar navbar-inverse navbar-static-top">
+     	<div class="container">
+        	<div class="navbar-header">
+          		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            		<span class="sr-only">Toggle navigation</span>
+            		<span class="icon-bar"></span>
+            		<span class="icon-bar"></span>
+            		<span class="icon-bar"></span>
+          		</button>
+          		<a class="navbar-brand" href="#">COSEPAR</a>
+        	</div>
             
+        <div id="navbar" class="navbar-collapse collapse navbar-right">
+          <ul class="nav navbar-nav">
+          	<li class="active"><a href="#">Inicio</a></li>
+            <li><a href="#about">Servicios</a></li>
+            <li><a href="#contact">Nosotros</a></li>
+            <li><a href="#contact">Ubicacion</a></li>
+            <li><a href="#contact">Contacto</a></li>
           </ul>
-          <ul class="nav navbar-nav navbar-right">
+          <!--<ul class="nav navbar-nav navbar-right">
             <!--<li><a href="../navbar/">Default</a></li>
             <li class="active"><a href="./">Static top <span class="sr-only">(current)</span></a></li>
-            <li><a href="../navbar-fixed-top/">Fixed top</a></li>-->
-          </ul>
+            <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+          </ul>-->
         </div><!--/.nav-collapse -->
       </div>
     </nav>
+  
+  
+  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  	<div class="carousel-enlaces">
+    <div class="container">
+        	<div class="row">
+        		<div class="col-sm-6">	
+                <h1>Probando columna 1</h1>
+        		<p> Leyenda columna 1</p>
+                </div>
+                <div class="col-sm-6">
+                <h1>Probando columna 2</h1>
+        		<p> Leyenda columna 2</p>
+                </div>
+        	</div>
+        </div>
+  </div>
+  <!-- Indicators -->
+  	<ol class="carousel-indicators">
+    	<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    	<li data-target="#myCarousel" data-slide-to="1"></li>
+    	<li data-target="#myCarousel" data-slide-to="2"></li>
     
+  	</ol>
+
+  	<!-- Wrapper for slides -->
+  	<div class="carousel-inner" role="listbox">
+    	
+        
+        <div class="item active">
+      		<img src="img/coop_1.jpg" alt="Chania">
+    	</div>
+    	<div class="item">
+      		<img src="img/coop_2.jpg" alt="Chania">
+    	</div>
+		<div class="item">
+	      <img src="img/coop_3.jpg" alt="Flower">
+  		</div>
+   	</div>
+
+  <!-- Left and right controls
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a> -->
+	</div>
+    
+    
+    <div class="cnt-mensajebienvenida">
+    	<div class="container" >
+    		<h1>Bienvenidos a COSEPAR Ltda.<h1>
+        	<h2>Cooperativa de servicios Publicos de Arata Ltda.</h2>
+    	</div>   
+    </div>
+
+ 	<div class="container">
+        	<div class="row">
+        		<div class="col-lg-6">	
+                <h1>Probando columna 1</h1>
+        		<p> Leyenda columna 1</p>
+                </div>
+                <div class="col-lg-6">
+                <h1>Probando columna 2</h1>
+        		<p> Leyenda columna 2</p>
+                </div>
+        	</div>
+    </div>
+     
+     
+     
      <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/jquery.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
     <script src="js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="js/ie10-viewport-bug-workaround.js"></script>
