adf94ed8f5a27f41a66b41da5a084ca058ae0fae
marcoscareggio <marcoscareggio2005@hotmail.com>
2017-05-30 19:22:02 -0300

    servicios agregados

--- index.php
+++ index.php
@@ -82,13 +82,13 @@
             		<span class="icon-bar"></span>
             		<span class="icon-bar"></span>
           		</button>
-          		<a class="navbar-brand" href="#">COSEPAR</a>
+          		<a class="navbar-brand" href="#"><img class="img-logo" src="img/icon/logo-coop_107x98_sinborde.png">COSEPAR</a>
         	</div>
             
         <div id="navbar" class="navbar-collapse collapse navbar-right">
           <ul class="nav navbar-nav">
           	<li class="active"><a href="#">Inicio</a></li>
-            <li><a href="#about">Servicios</a></li>
+            <li><a href="#Servicios">Servicios</a></li>
             <li><a href="#contact">Nosotros</a></li>
             <li><a href="#contact">Ubicacion</a></li>
             <li><a href="#contact">Contacto</a></li>
@@ -108,27 +108,27 @@
                 	<div class="enlaces">
                     <span class="cabecera-enlaces">Mail</span>
                         <ul class="list-unstyled lista-enlaces">
-                            <li><span class="iconos-enlaces icono-mail glyphicon glyphicon-envelope"></span><a href="http://mail.google.com" target="new">Gmail</a>
-                            <li><span class="iconos-enlaces icono-mail glyphicon glyphicon-envelope"></span><a href="http://www.hotmail.com" target="new">Hotmail</a>
-                            <li><span class="iconos-enlaces icono-mail glyphicon glyphicon-envelope"></span><a href="http://www.yahoo.com.ar" target="new">Yahoo</a> 
+                            <li><span class="iconos-enlaces icono-mail glyphicon glyphicon-envelope color-cosepar"></span><a href="http://mail.google.com" target="new">Gmail</a>
+                            <li><span class="iconos-enlaces icono-mail glyphicon glyphicon-envelope color-cosepar"></span><a href="http://www.hotmail.com" target="new">Hotmail</a>
+                            <li><span class="iconos-enlaces icono-mail glyphicon glyphicon-envelope color-cosepar"></span><a href="http://www.yahoo.com.ar" target="new">Yahoo</a> 
                         </ul>
                     </div>
                     
                     <div class="enlaces">
                     <span class="cabecera-enlaces">Bancos</span>
                         <ul class="list-unstyled lista-enlaces">
-                            <li><span class="iconos-enlaces icono-banco color-rojo glyphicon glyphicon-piggy-bank"></span><a href="https://www.bancodelapampa.com.ar" target="new">Banco de La Pampa</a>
-                            <li><span class="iconos-enlaces icono-banco color-azul	glyphicon glyphicon-piggy-bank"></span><a href="http://www.bna.com.ar" target="new">Banco de la Nacion</a>
-                            <li><span class="iconos-enlaces icono-banco color-celeste	glyphicon glyphicon-piggy-bank"></span><a href="https://www.bbvafrances.com.ar" target="new">BBVA</a> 
+                            <li><span class="iconos-enlaces icono-banco color-cosepar glyphicon glyphicon-piggy-bank"></span><a href="https://www.bancodelapampa.com.ar" target="new">Banco de La Pampa</a>
+                            <li><span class="iconos-enlaces icono-banco color-cosepar	glyphicon glyphicon-piggy-bank"></span><a href="http://www.bna.com.ar" target="new">Banco de la Nacion</a>
+                            <li><span class="iconos-enlaces icono-banco color-cosepar	glyphicon glyphicon-piggy-bank"></span><a href="https://www.bbvafrances.com.ar" target="new">BBVA</a> 
                         </ul>
                     </div>
                     
                     <div class="enlaces">
                     <span class="cabecera-enlaces">Redes</span>
                         <ul class="list-unstyled lista-enlaces">
-                            <li><span class="iconos-enlaces icono-facebook fa fa-facebook-official"></span><a href="http://www.facebook.com" target="new">Facebook</a>
-                            <li><span class="iconos-enlaces icono-twitter fa fa-twitter-square"></span><a href="www.twitter.com" target="new">Twitter</a>
-                            <li><span class="iconos-enlaces icono-youtube fa fa-youtube-square"></span><a href="http://www.youtube.com" target="new">Youtube</a> 
+                            <li><span class="color-cosepar iconos-enlaces icono-facebook fa fa-facebook-official "></span><a href="http://www.facebook.com" target="new">Facebook</a>
+                            <li><span class="color-cosepar iconos-enlaces icono-twitter fa fa-twitter-square"></span><a href="www.twitter.com" target="new">Twitter</a>
+                            <li><span class="color-cosepar iconos-enlaces icono-youtube fa fa-youtube-square"></span><a href="http://www.youtube.com" target="new">Youtube</a> 
                         </ul>
                         
                     </div>
@@ -140,11 +140,11 @@
                     <div class="enlaces">
                     <span class="cabecera-enlaces">Diarios</span>
                         <ul class="list-unstyled lista-enlaces">
-                            <li><span class="iconos-enlaces icono-facebook fa fa-newspaper-o"></span><a href="http://www.infopico.com" target="new">Infopico</a>
-                            <li><span class="iconos-enlaces icono-youtube fa fa-newspaper-o"></span><a href="http://www.maracodigital.net" target="new">Maraco igital</a>
-                            <li><span class="iconos-enlaces icono-facebook fa fa-newspaper-o"></span><a href="http://www.laarena.com.ar" target="new">La Arena</a>
-                            <li><span class="iconos-enlaces icono-twitter fa fa-newspaper-o"></span><a href="http://www.diariolareforma.com.ar" target="new">La Reforma</a>
-                            <li><span class="iconos-enlaces icono-youtube fa fa-newspaper-o"></span><a href="http://www.pagina12.com.ar" target="new">Pagina 12</a> 
+                            <li><span class="color-cosepar iconos-enlaces icono-facebook fa fa-newspaper-o"></span><a href="http://www.infopico.com" target="new">Infopico</a>
+                            <li><span class="color-cosepar iconos-enlaces icono-youtube fa fa-newspaper-o"></span><a href="http://www.maracodigital.net" target="new">Maraco igital</a>
+                            <li><span class="color-cosepar iconos-enlaces icono-facebook fa fa-newspaper-o"></span><a href="http://www.laarena.com.ar" target="new">La Arena</a>
+                            <li><span class="color-cosepar iconos-enlaces icono-twitter fa fa-newspaper-o"></span><a href="http://www.diariolareforma.com.ar" target="new">La Reforma</a>
+                            <li><span class="color-cosepar iconos-enlaces icono-youtube fa fa-newspaper-o"></span><a href="http://www.pagina12.com.ar" target="new">Pagina 12</a> 
                         </ul>
                         
                     </div>
@@ -177,7 +177,7 @@
   	<ol class="carousel-indicators">
     	<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
     	<li data-target="#myCarousel" data-slide-to="1"></li>
-    	<li data-target="#myCarousel" data-slide-to="2"></li>
+    	
     
   	</ol>
 
@@ -191,32 +191,139 @@
     	<div class="item">
       		<img src="img/coop_2.jpg" alt="Chania">
     	</div>
-		<div class="item">
-	      <img src="img/coop_3.jpg" alt="Flower">
-  		</div>
+		
    	</div>
 
 	</div>
     
     
-    <div class="cnt-mensajebienvenida">
+    <div class="cnt-mensajebienvenida fondo-seccion-negra">
     	<div class="container" >
-    		<h1>Bienvenidos a COSEPAR Ltda.<h1>
-        	<h2>Cooperativa de servicios Publicos de Arata Ltda.</h2>
-    	</div>   
-    </div>
-
- 	<div class="container">
-    <div class="cnt-servicios">
-        	<h1>Nuestros Servicios</h1>
-          <div class="row">
-        		<div class="col-lg-6">	
+        	<div class="row">
+	            <div class="col-md-2"><img class="img-logo-bienvenida" src="img/icon/logo-coop_213x195.png"></div>
+                <div class="col-md-10">
+                    <h1>Bienvenidos a COSEPAR Ltda.</h1>
+                    <h2>Cooperativa de Agua Potable, Vivienda, Consumo y Otros Servicios Publicos de Arata Ltda.</h2>
+                </div>
             </div>
-            <div class="col-lg-6">
-            </div>
-        	</div>
-     </div>     
+        </div>   
     </div>
+ 	<div class="container cnt-servicios">
+    <a name="Servicios">
+        		<div class="row">
+                <div class="col-sm-12">
+                	<h1>Nuestros Servicios</h1>
+                </div>
+                <div class="row">
+                    <div class="col-md-2">	
+                		<div class="servicio">
+                        	<a href="#Energia">
+                            <div class="serv-cara1">
+                            <div class="glyphicon glyphicon-flash icono-serv"></div>
+                            <div class="leyenda-serv">Energia Electrica</div>
+                            </div>
+                            <div class="serv-cara2">
+                            <div class="glyphicon glyphicon-flash icono-serv"></div>
+                            <div class="leyenda-serv">Energia Electrica</div>
+                            </div>
+                            </a>
+                        </div>
+                    </div>
+					<div class="col-md-2">	
+                		<div class="servicio agua">
+                        	<a href="#Agua">
+                        	<div class="serv-cara1">
+                            <div class="glyphicon glyphicon-tint icono-serv"></div>
+                            <div class="leyenda-serv">Agua Potable</div>
+                            </div>
+                            <div class="serv-cara2">
+                            <div class="glyphicon glyphicon-tint icono-serv"></div>
+                            <div class="leyenda-serv">Agua Potable</div>
+                            </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-2">	
+                		<div class="servicio tv">
+                        	<a href="#TV">
+                        	<div class="serv-cara1">
+                            <div class="fa fa-television  icono-serv"></div>
+                            <div class="leyenda-serv">TV Cable</div>
+                            </div>
+                            <div class="serv-cara2">
+                            <div class="fa fa-television icono-serv"></div>
+                            <div class="leyenda-serv">TV Cable</div>
+                            </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-2">	
+                		<div class="servicio internet">
+                        	<a href="#Internet">
+                        	<div class="serv-cara1">
+                            <div class="fa fa-wifi icono-serv"></div>
+                            <div class="leyenda-serv">Internet</div>
+                            </div>
+                            <div class="serv-cara2">
+                            <div class="fa fa-wifi icono-serv"></div>
+                            <div class="leyenda-serv">Internet</div>
+                            </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-2">	
+                		<div class="servicio sepelio">
+                        	<a href="#Sepelio">
+                        	<div class="serv-cara1">
+                            <div class="icono-serv">S</div>
+                            <div class="leyenda-serv">Sepelio</div>
+                            </div>
+                            <div class="serv-cara2">
+                            <div class="icono-serv">S</div>
+                            <div class="leyenda-serv">Sepelio</div>
+                            </div>
+                            </a>
+                        </div>
+                    </div>
+              		
+                
+                
+                      
+                </div>
+               
+        </div>  
+        </a>
+        </div>
+        <a name="Energia"></a>
+        <div class="cnt-energia fondo-seccion-negra">
+        <div class="container">
+        	<div class="row">
+            <div class="col-md-6">
+                <h1>Energia Eléctrica</h1>
+                <h4>La COSEPAR Ltda. presta servicion de distribucion de energía con una moderna red de baja tension y alumbrado público en la zona urbana.Tambien realiza la distribucion de energia con lineas de media tension en la zona rural de la localidad de Arata.La Energía distribuida a los usuarios es suministrada por la A.P.E.</h4>
+            </div>
+			<div class="col-md-6">
+            <img src="img/energia_electrica.jpg" class="img-energia">
+            </div>
+       	 </div>
+         </div>
+         </div>  
+        <a name="Agua"></a>
+		<div class="cnt-agua">
+        <div class="container">
+        	<div class="row">
+            <div class="col-md-6">
+           		 <img src="img/agua_potable.jpg" class="img-agua" >
+            </div>
+            <div class="col-md-6">
+                <h1>Agua Potable</h1>
+                <h4>COSEPAR Ltda. presta Servicio de Agua Potable en la zona urbana de la Localidad de Arata. El agua se entrae con cuatro bombas colocadas en los alrededores del pueblo.El agua es conducida hasta el pueblo para su potabilizacion y luego almacenda en una copa elevada para lograr su distribucion.</h4>
+            </div>
+			</div>
+        </div>
+        </div>    
+        </div>  
+    
      
      
      
@@ -233,8 +340,12 @@
       // Activate CarouselDiapositivas
 	  
 	  $( document ).ready(function() {
-    GetRSS('rss/test.rss');
-	
+    	GetRSS('rss/test.rss');
+		var ventana_alto = $(window).height();
+		var alto_div=ventana_alto-$(".carousel-enlaces").height();
+		//alert($(".carousel-enlaces").height());
+		$(".cnt-mensajebienvenida").css("height",alto_div+"px");
+		var ventana_alto = $(window).height();
 });
       $("#myCarousel").carousel({interval: 3000});
       // Enable Carousel Indicators
@@ -242,6 +353,7 @@
       $("#myCarousel").carousel(1);
 });
 	  //----Fin Carousel-Diapòsitivas
+	  
     </script>
   </body>
   
